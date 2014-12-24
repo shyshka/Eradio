beb22bb8cdda5d8bb062584883b37b8bfdeadba1
ww4ka <ww4ka@ukr.net>
2014-12-24 10:54:09 +0200

    Add new project in samples Реализовал чтение текущей песни и загрузнка картинки асинхронно.
    Signed-off-by: ww4ka <ww4ka@ukr.net>

---
+++ Samples/Request/Request/Request/BackThread.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Request
+{
+    public class BackThread
+    {
+        public  bool IsCanceled = false;
+        private const int interval = 10000;
+
+        public delegate void NowPlayEventHandler(object sender, NowPlay arg);
+        public  event NowPlayEventHandler NowPlayChanged;
+
+        private  NowPlay _nowPlay_obj;
+        private  NowPlay NowPlay_obj
+        {
+            get { return _nowPlay_obj; }
+            set
+            {
+                if (value == null) return;
+                if (_nowPlay_obj != null)
+                    if (_nowPlay_obj.Equals(value)) return;
+                _nowPlay_obj = value;
+                if (NowPlayChanged != null) NowPlayChanged(null, _nowPlay_obj);
+            }
+        }
+
+        public  void StartThread()
+        {
+            Thread thread = new Thread(new ThreadStart(delegate
+                {
+                    while (!IsCanceled)
+                    {
+                        NowPlay_obj = NowPlay.CreateNewObject();
+                        Thread.Sleep(interval);
+                    }
+                }));
+            thread.Start();
+        }
+    }
+}
---
+++ Samples/Request/Request/Request/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Request
+{
+    public partial class Form1 : Form
+    {
+        private BackThread _backThreadObj = new BackThread();
+
+        public Form1()
+        {
+            InitializeComponent();
+            _backThreadObj.StartThread();
+            _backThreadObj.NowPlayChanged += (obj, arg) =>
+            {
+                if (this.InvokeRequired) this.BeginInvoke(new BackThread.NowPlayEventHandler(ShowNowPlay), obj, arg);
+                else ShowNowPlay(obj, arg);
+            };
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void ShowNowPlay(object sender, NowPlay arg)
+        {
+            label1.Text = arg.ARTIST_NAME;
+            label2.Text = arg.TRACK_SONG;
+            pictureBox1.LoadAsync("http://eradio.ua/i/" + arg.PICTURE);
+        }
+    }
+}
